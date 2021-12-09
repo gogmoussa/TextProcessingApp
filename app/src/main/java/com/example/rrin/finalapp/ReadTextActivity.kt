083a28b0cb85c28a9c2df0377827c8cbe8a9bd9a
George Moussa <georgemoussa19197@gmail.com>
2021-12-08 19:54:58 -0500

    change text to resource string

--- app/src/main/java/com/example/rrin/finalapp/ReadTextActivity.kt
+++ app/src/main/java/com/example/rrin/finalapp/ReadTextActivity.kt
@@ -69,7 +69,7 @@ class ReadTextActivity : AppCompatActivity() {
                     textView.text = visionText.text
                 }
                 .addOnFailureListener { e ->
-                    textView.text = "Failed: could not read text"
+                    textView.text = getString(R.string.failed_translate)
                 }
         }
 
